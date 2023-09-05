8bbf8854e886e11d3a375fac4b5c843e6ee99401
Kelton Benson <kelton01@csu.fullerton.edu>
2023-09-05 16:27:25 -0700

    Initial Commit

---
+++ Live-Demo-Week_03-Example-IOS-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live-Demo-Week_03-Example-IOS-App
+//
+//  Created by kelton on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
